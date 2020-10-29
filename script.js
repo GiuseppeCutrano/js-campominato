77cf5b6b35b85f19ff9aa8d29df9e3904f12086b
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-10-29 13:51:41 +0100

    implementata gran parte della logica

--- script.js
+++ script.js
@@ -12,19 +12,44 @@
 // con difficoltà 2 => tra 1 e 50
 
 var bombe = []; //bombe
-var numeriUtente = []; //numeri inseriti dall'utente
-var listaNumUtente = []; //numeri inseriti dall'utente
-var score = 0; // punteggio
+var numeriUtente = []; //numeri inseriti dall'Utente
+var listaNumUtente = []; //lista dei numeri inseriti dall'Utente
+var scores = 0; // punteggio
 
 
-//creazione numeri
-for (var i = 0; i < 16; i++) {
-    var numRandom = Math.floor( Math.random() * 100) + 1;
-        if (bombe.includes(numRandom)) {
-          i--; }
-        else {
-          bombe.push(numRandom);
-        }
-}
 
-console.log(bombe);
+
+
+ while(bombe.length < 16){
+   var numRandom = Math.floor( Math.random() * 100) + 1;
+   if (bombe.includes(numRandom, bombe) == false) {
+       bombe.push(numRandom); }
+
+
+ }
+ console.log(bombe)
+
+var dead = false;
+ while (dead == false && scores < 84) {
+       console.log("Numero scelto: " + numeriUtente);
+       numeriUtente = parseInt(prompt("Scegli un numero da 1 a 100."));
+       if (numeriUtente > 0 && numeriUtente < 101 && !isNaN(numeriUtente)) {
+         
+         // valuto la possibilità che il numero inserito era stato già precedentemente introdotto
+         if (listaNumUtente.includes(numeriUtente) == true) {
+           alert("Errore! Numero già inserito.");
+           // valuto la possibilità che l utente trovi la mina
+         } else if (bombe.includes(numeriUtente) == true) {
+           alert("Hai perso");
+           dead = true;
+           listaNumUtente.push(numeriUtente);
+         } else {
+           alert("Sei ancora vivo!");
+           listaNumUtente.push(numeriUtente);
+           scores++;
+         }
+       }else {
+         alert('Il valore inserito è errato. Riprova.');
+       }
+     }
+     console.log('Numeri scelti: ' + numeriUtente);
